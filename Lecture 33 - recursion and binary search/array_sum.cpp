aba7089dce7e180ed559fdc75841941aa880165b
YashSakhareliya <sakhareliyayash@gmail.com>
2024-06-12 13:31:19 +0530

    sum of array using recursion

--- Lecture 33 - recursion and binary search/array_sum.cpp
+++ Lecture 33 - recursion and binary search/array_sum.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+// find arr sum using recursion
+
+int findsum2(int *arr,int size){
+    if(size==0)
+    return 0;
+    
+    if(size==1)
+    return arr[0];
+
+    return arr[0] + findsum2(arr+1,size-1);
+}
 int findsum(int arr[],int size, int sum){
 
     // base case
@@ -12,11 +23,14 @@ int findsum(int arr[],int size, int sum){
 }
 
 int main() {
-    int arr[5] = {0,0,0,0,0};
+    int arr[5] = {10,20,20,20,0};
 
     int size = 5;
 
     int sum = findsum(arr,size,0);
-    cout<<sum;
+    cout<<sum<<endl;
+
+    int sum2 = findsum2(arr,size);
+    cout<<sum2;
     return 0;
 }

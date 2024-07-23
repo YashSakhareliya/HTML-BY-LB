a1941a9b1ca8fa2936a7f7ee9f9960d2766af0d4
Yash sakhareliya <sakhareliyayash@gmail.com>
2024-07-23 16:20:36 +0530

    implement tree

---
+++ Lecture 62-Binary Tree/implementation.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int d){
+        this->data=d;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+Node* implement(Node* root){
+    cout<<"Enter the data"<<endl;
+    int data;
+    cin>>data;
+    root = new Node(data);
+    if(data == -1){
+        return NULL;
+    }
+
+    cout<<"Enter the data left of"<<data;
+    implement(root->left);
+    cout<<"Enetr the data right of "<<data;
+    implement(root->right);
+    return root;
+}
+
+int main() {
+    Node * root = NULL;
+    root = implement(root);
+    return 0;
+}

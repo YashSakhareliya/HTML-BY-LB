edbe6c1a61410aa14f00fd625ccfb226c55a3f04
Yash sakhareliya <sakhareliyayash@gmail.com>
2024-09-05 17:16:01 +0530

    create Graph using Map list and also creating graph using Vector

--- Lecture 85-Graph/creation_graph.cpp
+++ Lecture 85-Graph/creation_graph.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <list>
 #include<unordered_map>
+#include<vector>
 using namespace std;
 
+// adjence list
 class graph{
     public:
     unordered_map<int, list<int>> g;
@@ -29,6 +31,30 @@ class graph{
     }
 
 };
+class gVector{
+    public:
+    vector<vector<int>> g;
+    gVector(int node){
+        g.resize(node);
+    }
+
+    void addEdge(int u, int v){
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+
+    void PrintG(){
+        
+        for(int i=0;i<g.size();i++){
+            cout<<i<<"->";
+            for(int j=0;j<g[i].size();j++){
+                cout<<g[i][j]<<",";
+            }
+            cout<<endl;
+        }
+    }
+
+};
 int main() {
     int n;
     int m;
@@ -36,13 +62,20 @@ int main() {
     cin>>n;
     cout<<"Enter the edge";
     cin>>m;
-    graph g;
 
+    graph g; // graph using map and list
+
+    // in vector graph you give node from starting 0,1,2 ect.
+    gVector vec(n); // graph using list
+    
     for(int i=0; i<m; i++){
         int u,v;
         cin>>u>>v;
-        g.addEdge(u,v,0);
+        vec.addEdge(u,v); 
+        g.addEdge(u,v,0); 
     }
-    g.printGraph();
+    vec.PrintG(); // print vector graph
+    g.printGraph(); // print map and list graph 
+    
     return 0;
 }

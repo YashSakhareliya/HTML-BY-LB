1f35f3f429724e2acdba5de668ad972fcd929c91
Yash sakhareliya <sakhareliyayash@gmail.com>
2024-11-26 16:14:29 +0530

    Add Next Permutation code

---
+++ Array Striver/Next_Permutation.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    vector<int> v = {1, 2, 3};
+  
+    // Inbuild stl library 
+    
+    // next_permutation(v.begin(), v.end());
+    // for(auto it:v){
+    //     cout<<it;
+    // }
+
+
+    return 0;
+}
